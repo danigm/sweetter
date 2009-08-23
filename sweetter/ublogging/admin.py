31f3341a8e115507bde88162dc7fdace453dbb07
Christian Lopez Espinola <penyaskito@computer.org>
2009-08-23 21:15:26 +0200

    Added autoadmin for creating test data.

---
+++ sweetter/ublogging/admin.py
@@ -0,0 +1,8 @@
+from sweetter.ublogging.models import *
+from django.contrib import admin
+
+admin.site.register(User)
+admin.site.register(Group)
+admin.site.register(Role)
+admin.site.register(Membership)
+admin.site.register(Post)
