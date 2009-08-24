63daa414f0dea42cdb1695f3120735853b25f4eb
Christian Lopez Espinola <penyaskito@computer.org>
2009-08-24 23:26:23 +0200

    Use built-in authentication system.

--- sweetter/ublogging/admin.py
+++ sweetter/ublogging/admin.py
@@ -1,8 +1,6 @@
 from sweetter.ublogging.models import *
 from django.contrib import admin
 
-admin.site.register(User)
+admin.site.register(Profile)
 admin.site.register(Group)
-admin.site.register(Role)
-admin.site.register(Membership)
 admin.site.register(Post)
--- sweetter/ublogging/models.py
+++ sweetter/ublogging/models.py
@@ -1,12 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class User(models.Model):    
-    username = models.CharField(max_length=100,unique=True)
-    password = models.CharField(max_length=200)
-    date_join = models.DateField('join date')
+class Profile(models.Model):
+    user = models.ForeignKey(User)
     location = models.CharField(max_length=100)
     karma = models.DecimalField(max_digits=5, decimal_places=2)
-    email = models.EmailField(max_length=75)
     avatar = models.ImageField(upload_to='avatars')
     votes = models.PositiveIntegerField()
     
@@ -14,14 +12,6 @@ class Group(models.Model):
     groupname = models.CharField(max_length=100,unique=True)
     date_created = models.DateField('creation date')
     
-class Role(models.Model):
-    name = models.CharField(max_length=100);    
-
-class Membership(models.Model):
-    userid = models.ForeignKey(User)
-    groupid = models.ForeignKey(Group)
-    roleid = models.ForeignKey(Role)
-
 class Post(models.Model):
     userid = models.ForeignKey(User)
     text = models.CharField(max_length=140)
