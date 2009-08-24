859f3431d5b9f3e178b141817b261d31153b1092
Christian Lopez Espinola <penyaskito@computer.org>
2009-08-24 23:46:19 +0200

    Link profile to user accounts. Autocreate the profile object when a user is created.

--- sweetter/settings.py
+++ sweetter/settings.py
@@ -65,6 +65,8 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'sweetter.urls'
 
+AUTH_PROFILE_MODULE = 'ublogging.Profile'
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
--- sweetter/ublogging/models.py
+++ sweetter/ublogging/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, unique=True)
     location = models.CharField(max_length=100)
-    karma = models.DecimalField(max_digits=5, decimal_places=2)
+    karma = models.DecimalField(default=0, max_digits=5, decimal_places=2)
     avatar = models.ImageField(upload_to='avatars')
-    votes = models.PositiveIntegerField()
+    votes = models.PositiveIntegerField(default=0)
     
 class Group(models.Model):
     groupname = models.CharField(max_length=100,unique=True)
@@ -19,3 +19,14 @@ class Post(models.Model):
 
     def __unicode__(self):
         return self.text
+
+# SIGNALS AND LISTENERS
+from django.contrib.auth.models import User
+
+
+# User
+def user_post_save(sender, instance, signal, *args, **kwargs):
+    # Creates user profile
+    profile, new = Profile.objects.get_or_create(user=instance)
+
+models.signals.post_save.connect(user_post_save, sender=User)
