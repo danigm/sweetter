62a4635dc93d9826625b37b8e576cde7acbf8317
Christian Lopez Espinola <penyaskito@computer.org>
2009-08-25 21:46:48 +0200

    RequestContext is required for the context_processors.auth processor. Without it, templates don't know the current status of the request.

--- sweetter/ublogging/views.py
+++ sweetter/ublogging/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.contrib.auth.forms import UserCreationForm
 from sweetter.ublogging.models import Post, User
 import datetime
 
 def index(request):
     latest_post_list = Post.objects.all().order_by('-pub_date')[:10]
-    return render_to_response('status/index.html', {'latest_post_list': latest_post_list})
+    return render_to_response('status/index.html', {
+            'latest_post_list': latest_post_list
+        }, context_instance=RequestContext(request))
 
 def new(request):
     text = request.POST['text']
@@ -25,15 +28,15 @@ def join(request):
             return render_to_response('join.html', {
                 'error_message': errormessage,
                 'form': form,
-            })
+            }, context_instance=RequestContext(request))
         else:
             return render_to_response('join.html', {
                 'error_message': "Error de validacion",
                 'form': form
-            })
+            }, context_instance=RequestContext(request))
     else:
         form = UserCreationForm()
         return render_to_response('join.html', {
             'form': form,
-        })
+        }, context_instance=RequestContext(request))
     
