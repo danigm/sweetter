b676b5ce042a4a2e1a3ebfbcbe5706c35edb4f80
Luis Manuel García Conde <bigchuvi@gmail.com>
2009-09-19 21:22:00 +0200

    Added link to Join/Leave a group in group details page

--- sweetter/contrib/groups/views.py
+++ sweetter/contrib/groups/views.py
@@ -4,14 +4,32 @@ from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.forms import UserCreationForm
-from sweetter.ublogging.models import Post, User
+from sweetter.ublogging.models import Post, User, Profile
 from sweetter.contrib.groups.models import Group
 import datetime
-'''
-def index(request):
-    groups_list = Group.objects.all()
-    return render_to_response('allgroups.html', {
-            'object_list': groups_list
+
+def detail(request,object_id):
+    g = Group.objects.get(id=object_id)
+    if g.users.filter(user=request.user):
+        join_message = u'Leave'
+    else:
+        join_message = u'Join'
+    return render_to_response('groups/detail.html', {
+            'object': g, 'join_message':join_message
         }, context_instance=RequestContext(request))
         
-        '''
+def join(request,group_name):
+    g = Group.objects.get(name=group_name)
+    try:
+        u = g.users.get(user=request.user)
+        g.users.remove(u)
+        join_message = u'Join'
+    except:
+        p = Profile.objects.get(user=request.user)
+        g.users.add(p)
+        join_message = u'Leave'
+    g.save()
+    return render_to_response('groups/detail.html', {
+            'object': g, 'join_message':join_message
+        }, context_instance=RequestContext(request))
+        
