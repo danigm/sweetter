95fc53c9ae581664c243be044df08690592894c0
danigm <dani@danigm.net>
2009-10-01 15:55:25 +0200

    Jquery counter and some styles

---
+++ sweetter/static/js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+ $("#text").keyup(function() {
+     var input = $("#text")[0].value;
+     var max = $("#hc")[0].innerHTML;
+     updateStatusTextCharCounter(input, max);
+ });
+});
+
+function updateStatusTextCharCounter(value, max) {
+    len = value.length;
+    res = max - len;
+    jQuery('#counter').html('' + res);
+    if (len > max) {
+        if (jQuery("#new").attr('disabled') != 'disabled') {
+            jQuery('#new').attr('disabled', 'disabled');
+        }   
+    } else {
+        if (jQuery("#new").attr('disabled') == true) {
+            jQuery('#new').removeAttr('disabled');
+        }   
+
+        if (res > 10) {
+            jQuery('#counter').css('color', '#999' );
+        } else if (res > 5) {
+            jQuery('#counter').css('color', '#940000' );
+        } else {
+            jQuery('#counter').css('color', '#f00' );
+        }   
+    }   
+}
+
