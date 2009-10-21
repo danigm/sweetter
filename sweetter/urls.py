d816f4a3a0de67b46db4e2e3cf010c28cd606849
danigm <dani@danigm.net>
2009-10-22 01:04:33 +0200

    removed unnecesary comments

--- sweetter/urls.py
+++ sweetter/urls.py
@@ -1,19 +1,11 @@
 from django.conf.urls.defaults import *
 import settings
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
     (r'^sweetter/', include('sweetter.ublogging.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
     (r'^login/$','django.contrib.auth.views.login',{'template_name': 'login.html'}),
     (r'^logout/$','django.contrib.auth.views.logout'),
